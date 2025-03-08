d0fa7444a13af981429004294ccc073390718d18
Øyvind Solem <103931533+Aegers86@users.noreply.github.com>
2025-03-08 19:46:37 +0100

    Teste litt tkinter

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+import tkinter as tk #Importerer tkinter
+from tkinter import ttk #Importerer ttk fra tkinter
+
+root = tk.Tk() #Lager et vindu
+root.geometry("800x500") #Setter størrelsen på vinduet
+root.title("Tverrfaglig prosjekt") #Setter tittelen på vinduet
+
+#Lager en label
+label = tk.Label(root, text="Velkommen til tverrfaglig prosjekt", font=("Arial", 18)) #Lager en label
+label.pack(padx=10, pady=10) #Plasserer labelen i vinduet
+
+#lage en tekstboks
+textbox = tk.Text(root, height=10, width=50, font=("Arial", 14)) #Lager en tekstboks
+textbox.pack(padx=10, pady=10) #Plasserer tekstboksen i vinduet
+
+#Lager en knapp
+button = tk.Button(root, text="Klikk her", font=("Arial", 14)) #Lager en knapp
+button.pack(padx=10, pady=10) #Plasserer knappen i vinduet
+
+root.mainloop() #Starter grensesnittet
