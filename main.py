976dcdb42cf2989eaefbba85a5ed8beb1a32a56c
Øyvind Solem <103931533+Aegers86@users.noreply.github.com>
2025-03-09 18:29:17 +0100

    Lagt inn database connection i egen metode, laget flere metoder for å hente info.

--- main.py
+++ main.py
@@ -6,21 +6,12 @@ from dotenv import load_dotenv #Importerer load_dotenv fra dotenv
 import os 
 
 load_dotenv() #Laster inn .env filen og henter informasjonen fra .env filen så vi slipper å eksponere passord og brukernavn
-#Husk og lage en fil som heter .gitignore og legg til .env i denne filen slik at .env filen ikke blir lastet opp til github
+#Husk og lage en fil som heter .gitignore(Ser ut som den kommer med på pull request) og legg til .env i denne filen slik at .env filen ikke blir lastet opp til github
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-
-#Mysql connector som bruker informasjonen fra .env filen
-db = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    passwd=DB_PASSWORD,
-    port=DB_PORT,
-    database=DB_NAME
-    )
 
 #Klasse for GUI
 class GUI:
@@ -29,16 +20,12 @@ class GUI:
         self.root.geometry("800x1000") #Setter størrelsen på vinduet
         self.root.title("Tverrfaglig prosjekt") #Setter tittelen på vinduet
 
-        #Lager en label
-        self.label = tk.Label(self.root, text="Velkommen til tverrfaglig prosjekt", font=("Arial", 18)) #Lager en label
-        self.label.pack(padx=10, pady=10) #Plasserer labelen i vinduet
-
-        #lage en tekstboks
-        self.textbox = tk.Text(self.root, height=10, width=50, font=("Arial", 14)) #Lager en tekstboks
-        self.textbox.pack(padx=10, pady=10) #Plasserer tekstboksen i vinduet
+        #Lager en knapp
+        self.button = tk.Button(self.root, text="Vis varer på lager", font=("Arial", 14), command=self.hentVarerPåLager) #Lager en knapp
+        self.button.pack(padx=10, pady=10) #Plasserer knappen i vinduet
 
         #Lager en knapp
-        self.button = tk.Button(self.root, text="Klikk her", font=("Arial", 14), command=self.test_knapp) #Lager en knapp
+        self.button = tk.Button(self.root, text="Vis alle ordre", font=("Arial", 14), command=self.hentAlleOrdrer) #Lager en knapp
         self.button.pack(padx=10, pady=10) #Plasserer knappen i vinduet
 
         #label over liste over varer på lager
@@ -46,15 +33,20 @@ class GUI:
         self.label.pack(padx=10, pady=10) #Plasserer labelen i vinduet
 
         #Lager ett tre for å vise alle varer som er på lager
-        self.colums = ("VNr", "Betegnelse", "Pris", "Antall") #Kolonnene i treet
-        self.tree = ttk.Treeview(self.root, columns=self.colums, show="headings")
+        self.tree = ttk.Treeview(self.root, show="headings")
         self.tree.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-        for col in self.colums:
-            self.tree.heading(col, text=col)
-            self.tree.column(col, width=100, anchor="center")
+        #Koble click event til treet
+        self.tree.bind("<Double-1>", self.påTreKlikk)
+           
+        self.root.protocol("WM_DELETE_WINDOW", self.terminate) #Lukker vinduet/avslutter programmet
+        self.root.mainloop() #Starter grensesnittet
 
-        #Henter data fra databasen og legger det inn i treet
+    def terminate(self):
+        if messagebox.askyesno("Avslutt", "Er du sikker på at du vil avslutte?"): #Bekreftelse på at bruker vil avslutte
+            self.root.destroy()
+
+    def databaseConnection(self): #En metode for å koble til databasen
         self.db = mysql.connector.connect(
             host=DB_HOST,
             user=DB_USER,
@@ -62,21 +54,59 @@ class GUI:
             port=DB_PORT,
             database=DB_NAME
         )
+    
+    def tømTre(self): #Tømmer treet
+        for i in self.tree.get_children():
+            self.tree.delete(i)
+        
+    def oppdaterKolonner(self, kolonner): #Oppdaterer kolonnene i treet
+        self.tree["columns"] = kolonner #Setter kolonnene i treet
+        for col in kolonner: #Legger til kolonnene i treet
+            self.tree.heading(col, text=col)
+            self.tree.column(col, width=100, anchor="center")
+
+    def hentVarerPåLager(self): #Henter data fra databasen og legger det inn i treet
+        self.tømTre()
+        self.oppdaterKolonner(("VNr", "Betegnelse", "Pris", "Antall")) 
+        self.databaseConnection()
         c = self.db.cursor()
         c.execute("SELECT * FROM vare WHERE antall > 0 ORDER BY antall DESC;") #order by funker ikke for en eller annen grunn, sjekkes.
         data = c.fetchall()
-        for rec in data:
-            self.tree.insert("", "end", values=rec)
+        for i in data:
+            self.tree.insert("", "end", values=i)
         c.close()
-            
-        self.root.protocol("WM_DELETE_WINDOW", self.terminate) #Lukker vinduet/avslutter programmet
-        self.root.mainloop() #Starter grensesnittet
 
-    def test_knapp(self):
-        print("Knappen er trykket på")
+    def hentAlleOrdrer(self):
+        #Tømmer treet
+        self.tømTre()
+        self.oppdaterKolonner(("OrdreNr", "OrdreDato", "SendtDato", "BetaltDato", "KNr")) 
 
-    def terminate(self):
-        if messagebox.askyesno("Avslutt", "Er du sikker på at du vil avslutte?"):
-            self.root.destroy()
+        #Henter data fra databasen og legger det inn i treet
+        self.databaseConnection()
+        c = self.db.cursor()
+        c.execute("SELECT * FROM ordre;") #order by funker ikke for en eller annen grunn, sjekkes.
+        data = c.fetchall()
+        for i in data:
+            self.tree.insert("", "end", values=i)
+        c.close()
+
+    def visInfoOmOrdre(self, ordreNr):
+        #Tømmer treet
+        self.tømTre()
+        self.oppdaterKolonner(("OrdreNr","VNr", "PrisPrEnhet", "Antall")) 
+
+        #Henter data fra databasen og legger det inn i treet
+        self.databaseConnection()
+        c = self.db.cursor()
+        c.execute(f"SELECT * FROM ordrelinje WHERE OrdreNr = {ordreNr};") #Må legge inn join fra kunde, legge sammen ordrelinjer og total pris på ordre
+        data = c.fetchall()
+        for i in data:
+            self.tree.insert("", "end", values=i)
+        c.close()
+
+    def påTreKlikk(self, _): #Henter data fra treet og viser informasjon om ordrenummeret
+        selected_item = self.tree.selection()[0]
+        ordreNr = self.tree.item(selected_item, "values")[0]
+        self.visInfoOmOrdre(ordreNr)
 
 GUI() #Kjører programmet
