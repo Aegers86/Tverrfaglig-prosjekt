4ee4a7061d3529fcc4ddb79d5485fccc54b12cc1
merknu <Merodningen.Design@gmail.com>
2025-04-27 22:24:53 +0200

    * Lagt til funksjonalitet for å legge til en sjekk om en is_active er i tabellen, om ikke så legger vi den til.     * Begrenser sjekk via config.py for ryddigere logikk.     * Lagt til mer i validators.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # main.py
 from gui.hoved_vindu import MainWindow
+from database.database_checker import sjekk_og_opprett_is_active, sjekk_og_opprett_faktura_tabell
 
 def main():
+    try:
+        sjekk_og_opprett_is_active()
+        sjekk_og_opprett_faktura_tabell()  # 👈 utvidet sjekk
+    except Exception as e:
+        print(f"Feil ved databasetilpasning: {e}")
+        return  # Stopper programmet hvis det er kritiske feil
+
     app = MainWindow()
     app.run()
 
